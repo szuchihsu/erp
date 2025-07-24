63a2d43f462bc03041f99851b5cbca7c3aeb5ccf
Charlie Hsu <szuchi.hsu@student.mahidol.edu>
2025-07-24 22:02:23 +0800

    Modify Dashboard controller test

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get dashboard_index_url
+    get dashboard_url  # NOT dashboard_index_url
+    assert_response :success
+  end
+
+  test "should get root" do
+    get root_url  # Also works since root points to dashboard#index
     assert_response :success
   end
 end
