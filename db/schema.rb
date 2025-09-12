49bcb8ac153144ff881a8ba5e71705359d26c68e
Charlie Hsu <szuchi.hsu@student.mahidol.edu>
2025-09-12 19:57:57 +0800

    Remove material and weight fields from product schema and sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_071010) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_12_083245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -210,8 +210,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_071010) do
     t.string "name"
     t.text "description"
     t.string "category"
-    t.string "material"
-    t.decimal "weight"
     t.decimal "cost_price"
     t.decimal "selling_price"
     t.integer "stock_quantity"
